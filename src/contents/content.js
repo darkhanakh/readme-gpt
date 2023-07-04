fd92bc5e7d788a7cc29e6cc2c8ac8baa3c3535cd
Darkhan <darkhanahmetov228@gmail.com>
2023-07-04 14:01:09 +0600

    feat & refact: refactored content script to more readable code and added Github editor integration

--- src/contents/content.js
+++ src/contents/content.js
@@ -7,89 +7,44 @@ let editor = null;
 
 window.addEventListener("load", () => {
   setTimeout(() => {
-    editor = document.querySelector(".editor__inner");
+    editor = document.querySelector(".cm-content");
+    editor.textContent = "";
     console.log(editor);
   }, 500);
   console.log("Hello from script.js");
 });
 
-// (async () => {
-//   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     openAIUtils
-//       .generateProjectReadme(request.name, request.description)
-//       .then(async (response) => {
-//         const reader = response.body.getReader();
-//
-//         let chunks = "";
-//         let receivedLength = 0;
-//
-//         while (true) {
-//           const { done, value } = await reader.read();
-//
-//           if (done) {
-//             break;
-//           }
-//
-//           const chunkText = new TextDecoder("utf-8").decode(value);
-//           chunks += chunkText;
-//           receivedLength += chunkText.length;
-//         }
-//
-//         // Split the chunks by the "data: " prefix
-//         const chunkArray = chunks
-//           .split("data: ")
-//           .filter((chunk) => chunk.trim() !== "");
-//
-//         // Process the chunks here
-//         for (let i = 0; i < chunkArray.length - 1; i++) {
-//           const chunkText = chunkArray[i];
-//
-//           try {
-//             const jsonChunk = JSON.parse(chunkText);
-//             console.log(jsonChunk);
-//             // Access the desired data from the chunk and update the editor accordingly
-//             const content = jsonChunk.choices[0].delta.content;
-//             editor.innerHTML += content;
-//           } catch (error) {
-//             console.error("Error parsing JSON:", error);
-//             console.log("JSON data:", chunkText);
-//           }
-//         }
-//       });
-//   });
-// })();
+async function generateReadme(request, sender, sendResponse) {
+  const response = await openAIUtils.generateProjectReadme(
+    request.name,
+    request.description
+  );
+  const reader = response.body.getReader();
+  let text = "";
+  while (true) {
+    const { done, value } = await reader.read();
 
-(async () => {
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    openAIUtils
-      .generateProjectReadme(request.name, request.description)
-      .then(async (response) => {
-        const reader = response.body.getReader();
-        let text = "";
-        while (true) {
-          const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
 
-          if (done) {
-            break;
-          }
+    const chunkText = new TextDecoder("utf-8").decode(value);
 
-          const chunkText = new TextDecoder("utf-8").decode(value);
+    // Split the chunks by the "data: " prefix
+    const chunkArray = chunkText
+      .split("data: ")
+      .filter((chunk) => chunk.trim() !== "");
 
-          // Split the chunks by the "data: " prefix
-          const chunkArray = chunkText
-            .split("data: ")
-            .filter((chunk) => chunk.trim() !== "");
+    // Process the chunks here
+    const fetchedContent = chunkArray.reduce((acc, chunk) => {
+      const jsonChunk = JSON.parse(chunk);
+      const content = jsonChunk.choices[0].delta.content;
+      acc += content;
+      return acc;
+    }, "");
+    text += fetchedContent;
+    editor.textContent = text;
+  }
+}
 
-          // Process the chunks here
-          const fetchedContent = chunkArray.reduce((acc, chunk) => {
-            const jsonChunk = JSON.parse(chunk);
-            const content = jsonChunk.choices[0].delta.content;
-            acc += content;
-            return acc;
-          }, "");
-          text += fetchedContent;
-          editor.textContent = text;
-        }
-      });
-  });
-})();
+chrome.runtime.onMessage.addListener(generateReadme);
