cc550cfaed9509434ca0be81b543b995ea7faead
Darkhan <darkhanahmetov228@gmail.com>
2023-07-14 15:24:18 +0600

    fix: fixed prompt role

--- src/utils/OpenAIUtils.js
+++ src/utils/OpenAIUtils.js
@@ -40,11 +40,11 @@ export class OpenAIChatUtils {
   ) {
     const messages = [
       {
-        role: "You are developer that writes best README.md files",
+        role: "system",
         content: `You are a developer working on a project called ${projectName}.You need to write a concise and informative project README.md file to introduce the project and provide essential information to users. Also you can include html elements with center classes to provide more beautiful README. Add logos and gif where appropriate from repository resources, resources: ${
           resources ? resources : "No resources, do not include images"
         }. Extra information from user: ${extra}
-Write a README.md file in Markdown syntax and emojis, so document that covers the following sections:
+Write a README.md file in Markdown syntax and EMOJIS, so document that covers the following sections:
 Introduction: Provide a description to the project, explaining its purpose and goals.
 Features: Highlight the key features and functionalities of the project. Explain what sets it apart and makes it useful for users. With this features: ${features}
 Installation: Explain how users can install and set up the project on their local machines. Environment: ${projectEnvironment}
