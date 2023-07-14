af9cc246814c5f280eac470b0ae13083c4891c96
Darkhan <darkhanahmetov228@gmail.com>
2023-07-14 17:24:04 +0600

    fix: fixed prompt to more proper version

--- src/utils/OpenAIUtils.js
+++ src/utils/OpenAIUtils.js
@@ -14,6 +14,7 @@ export class OpenAIChatUtils {
       messages: messages,
       stream: true,
       max_tokens: 1000,
+      temperature: 1,
     };
 
     try {
@@ -41,19 +42,11 @@ export class OpenAIChatUtils {
     const messages = [
       {
         role: "system",
-        content: `You are a developer working on a project called ${projectName}.You need to write a concise and informative project README.md file to introduce the project and provide essential information to users. Also you can include html elements with center classes to provide more beautiful README. Add logos and gif where appropriate from repository resources, resources: ${
-          resources ? resources : "No resources, do not include images"
-        }. Extra information from user: ${extra}
-Write a README.md file in Markdown syntax and EMOJIS, so document that covers the following sections:
-Introduction: Provide a description to the project, explaining its purpose and goals.
-Features: Highlight the key features and functionalities of the project. Explain what sets it apart and makes it useful for users. With this features: ${features}
-Installation: Explain how users can install and set up the project on their local machines. Environment: ${projectEnvironment}
-Usage: Describe how users can use the project and any relevant instructions or examples to help them get started.
-Contributing: Provide guidelines and instructions for users who wish to contribute to the project. Explain how they can get involved, submit bug reports, or suggest improvements. Open to contribution: ${
-          contribution ? "Yes" : "No"
-        }
-License: Specify the license under which the project is distributed and any relevant terms or conditions. License: ${projectLicense}
-`,
+        content: "You are developer that writes best README.md files",
+      },
+      {
+        role: "user",
+        content: `You are a developer working on a project called **${projectName}**.\n\n      You need to write a concise and informative project README.md file to introduce the project and provide essential information to users. Use Markdown syntax and HTML elements with aligning classes like center and emojis to make your README more readable and engaging. Also include resources like gif and images from here: ${resources}. Extra information from user that you should use: ${extra}\n\n      The README should include the following sections: **Introduction:** Provide a description to the project, explaining its purpose and goals. **Features:** Highlight the key features and functionalities of the project. Explain what sets it apart and makes it useful for users. ${features} **Installation:** Explain how users can install and set up the project on their local machines. **Usage:** Describe how users can use the project and any relevant instructions or examples to help them get started. ${projectEnvironment} **Contributing:** Provide guidelines and instructions for users who wish to contribute to the project. Explain how they can get involved, submit bug reports, or suggest improvements. Is project open to contribution: ${contribution} **License:** Specify the license under which the project is distributed and any relevant terms or conditions. License: ${projectLicense}\n`,
       },
     ];
 
