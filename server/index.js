774ee34a7d6ac86e774a1dce802ede0d1143dcbc
Darkhan <darkhanahmetov228@gmail.com>
2023-07-18 14:11:35 +0600

    chore: added vite react client frontend for extension Github login

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+const bodyParser = require("body-parser");
+
+const app = express();
